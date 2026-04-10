84d702fa6585c8dedd28d36a1e45da7a3265111b
nwachee <fuzzy245.in@gmail.com>
2026-04-10 12:27:13 +0100

    feat: add Sepolia testnet support to RainbowKit chain config

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { anvil, zksync, mainnet, bsc } from "wagmi/chains";
+import { anvil, zksync, mainnet, bsc, sepolia } from "wagmi/chains";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { env } from "process";
 
 export default getDefaultConfig({
   appName: "BatchSend",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [anvil, zksync, mainnet, bsc],
+  chains: [anvil, zksync, mainnet, bsc, sepolia],
   ssr: false,
 });
